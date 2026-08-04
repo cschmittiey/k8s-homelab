4f98719c6ce203a6f05731412f7f60392899176c
Ellie Smith <ellie@ellie.mx>
2026-08-04 00:37:24 -0700

    switch to nix environment instead of brew

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "k8s-homelab workstation dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            age
+            cloudflared
+            fluxcd
+            go-task
+            kubernetes-helm
+            helmfile
+            jq
+            kubeconform
+            kubectl
+            kustomize
+            moreutils
+            sops
+            stern
+            talhelper
+            talosctl
+            yq-go
+            git
+          ];
+        };
+      }
+    );
+}
